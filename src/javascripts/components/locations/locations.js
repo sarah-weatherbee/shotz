7d4e2317eb17f2ab1f56db765df5e72aa0cb7cf7
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-04-22 19:28:08 -0500

    colors

--- src/javascripts/components/locations/locations.js
+++ src/javascripts/components/locations/locations.js
@@ -5,11 +5,31 @@ import './locations.scss';
 
 let locations = [];
 
+const shootTimeClass = (shootTime) => {
+  let selectedClass = '';
+  switch (shootTime) {
+    case 'Morning':
+      selectedClass = 'bg-secondary';
+      break;
+    case 'Afternoon':
+      selectedClass = 'bg-success';
+      break;
+    case 'Evening':
+      selectedClass = 'bg-info';
+      break;
+    case 'After Dark':
+      selectedClass = 'bg-danger';
+      break;
+    default:
+  }
+  return selectedClass;
+};
+
 const domStringBuilder = () => {
   let domString = '';
   locations.forEach((location) => {
     domString += `<div class="card location col-2" id=${location.id}>`;
-    domString += `<h3 class="card-header">${location.name}</h3>`;
+    domString += `<h3 class="card-header ${shootTimeClass(location.shootTime)}">${location.name}</h3>`;
     domString += '<div class="card-body">';
     domString += `<img src="${location.imageUrl}" class="card-img-top" alt="...">`;
     domString += `<h5>${location.address}</h5>`;
